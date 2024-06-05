07149fce8ae6df65adb64ab81b8a33c80e36703c
Nathalia Alencastro <nathalia.alencastro00@gmail.com>
2024-06-05 11:24:33 -0300

    mais capenga
    Co-Authored-By: Anna Lia Lira <134722036+annalialira@users.noreply.github.com>

--- CalculadoraFormas.java
+++ CalculadoraFormas.java
@@ -1,20 +1,19 @@
 // Classe para cálculos de formas geométricas especiais
 
 class CalculadoraFormas {
-    static final double PI = 3.14;
 
     // calcular área de um quadrado
-    static double calcularAreaQuadrado(double lado) {
+    public double calcularAreaQuadrado(double lado) {
         return lado * lado;
     }
 
     // Método para calcular área de um retângulo
-    static double calcularAreaRetangulo(double base, double altura) {
+    public double calcularAreaRetangulo(double base, double altura) {
         return base * altura;
     }
 
     // Método para calcular área de um triângulo
-    static double calcularAreaTriangulo(double base, double altura) {
+    public double calcularAreaTriangulo(double base, double altura) {
         return (base * altura) / 2;
     }
 
@@ -24,17 +23,17 @@ class CalculadoraFormas {
     }
 
     // Método para calcular área de um trapézio
-    static double calcularAreaTrapezio(double baseMaior, double baseMenor, double altura) {
+    public double calcularAreaTrapezio(double baseMaior, double baseMenor, double altura) {
         return ((baseMaior + baseMenor) * altura) / 2;
     }
 
     // Método para calcular área de um losango
-    static double calcularAreaLosangulo(double diagonalMaior, double diagonalMenor) {
+    public double calcularAreaLosango(double diagonalMaior, double diagonalMenor) {
         return (diagonalMaior * diagonalMenor) / 2;
     }
 
     // Método para calcular área de um paralelogramo
-    static double calcularAreaParalelogramo(double base, double altura) {
+    public double calcularAreaParalelogramo(double base, double altura) {
         return base * altura;
     }
 
--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 // executar
-public class Main extends CalculadoraFormas {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        CalculadoraFormas geometriaPlana = new CalculadoraFormas();
+        FormaGeometricaEspacial geometricaEspacial = new FormaGeometricaEspacial(0, 0, 0);
         //CalculadoraFormas calculadoraFormas = new CalculadoraFormas();
         //FormaGeometrica formaGeometrica = new FormaGeometrica();
         //FormaGeometricaEspacial formaGeometricaEspacial = new FormaGeometricaEspacial();
@@ -29,66 +31,74 @@ public class Main extends CalculadoraFormas {
                 System.out.println("5. Trapézio");
                 System.out.println("6. Losango");
                 System.out.println("7. Paralelogramo");
+                System.out.println("8. Círculo");
                 int opcao1 = scanner.nextInt();
                 System.out.println("Forma escolhida: "+opcao1+".");
+
+                double diagonalMaior, diagonalMenor, raio;
 
                 switch (opcao1) {
                     case 1:
                     System.out.println("Digite o valor do lado:");
-                    double base = scanner.nextDouble();
+                    double lado = scanner.nextDouble();
 
-                    CalculadoraFormas calcularAreaQuadrado = new CalculadoraFormas(base);
-                    System.out.println("A área da forma plana é: " + super.calcularAreaQuadrado);
+                    CalculadoraFormas calcularAreaQuadrado = new CalculadoraFormas();
+                        System.out.println("A área da forma plana é: " + geometriaPlana.calcularAreaQuadrado(lado));
                         break;
 
+        
                     case 2:
-                    System.out.println("Digite o valor do lado:");
+                    System.out.print("Digite a base do retângulo: ");
                     double base = scanner.nextDouble();
-
-                    CalculadoraFormas calcularAreaQuadrado = new CalculadoraFormas(base);
-                    System.out.println("A área da forma plana é: " + super.calcularAreaQuadrado);   
+                    System.out.print("Digite a altura do retângulo: ");
+                    double altura = scanner.nextDouble();
+                        System.out.println("Área do retângulo: " + geometriaPlana.calcularAreaRetangulo(base, altura));
                         break;
 
                     case 3:
-                    System.out.println("Digite o valor do lado:");
-                    double base = scanner.nextDouble();
-
-                    CalculadoraFormas calcularAreaQuadrado = new CalculadoraFormas(base);
-                    System.out.println("A área da forma plana é: " + super.calcularAreaQuadrado);   
+                    System.out.print("Digite a base do triângulo: ");
+                    base = scanner.nextDouble();
+                    System.out.print("Digite a altura do triângulo: ");
+                    altura = scanner.nextDouble();
+                    System.out.println("Área do triângulo: " + geometriaPlana.calcularAreaTriangulo(base, altura));
                         break;
                     
                     case 4:
-                    System.out.println("Digite o valor do lado:");
-                    double base = scanner.nextDouble();
-
-                    CalculadoraFormas calcularAreaQuadrado = new CalculadoraFormas(base);
-                    System.out.println("A área da forma plana é: " + super.calcularAreaQuadrado);   
+                    System.out.print("Digite o lado do triângulo equilátero: ");
+                    lado = scanner.nextDouble();
+                    System.out.println("Área do triângulo equilátero: " + geometriaPlana.calcularAreaTrianguloEquilatero(lado));
                         break;
 
                     case 5:
-                    System.out.println("Digite o valor do lado:");
-                    double base = scanner.nextDouble();
-
-                    CalculadoraFormas calcularAreaQuadrado = new CalculadoraFormas(base);
-                    System.out.println("A área da forma plana é: " + super.calcularAreaQuadrado);   
-                        break;
+                    System.out.print("Digite a base maior do trapézio: ");
+                    base = scanner.nextDouble();
+                    System.out.print("Digite a base menor do trapézio: ");
+                    double baseMenor = scanner.nextDouble();
+                    System.out.print("Digite a altura do trapézio: ");
+                    altura = scanner.nextDouble();
+                    System.out.println("Área do trapézio: " + geometriaPlana.calcularAreaTrapezio(base, baseMenor, altura));
 
                     case 6:
-                    System.out.println("Digite o valor do lado:");
-                    double base = scanner.nextDouble();
+                    System.out.print("Digite a diagonal maior do losango: ");
+                    diagonalMaior = scanner.nextDouble();
+                    System.out.print("Digite a diagonal menor do losango: ");
+                    diagonalMenor = scanner.nextDouble();
+                    System.out.println("Área do losango: " + geometriaPlana.calcularAreaLosango(diagonalMaior, diagonalMenor));
+                    break;
 
-                    CalculadoraFormas calcularAreaQuadrado = new CalculadoraFormas(base);
-                    System.out.println("A área da forma plana é: " + super.calcularAreaQuadrado);    
-                        break;
-                    
                     case 7:
-                    System.out.println("Digite o valor do lado:");
-                    double base = scanner.nextDouble();
+                    System.out.print("Digite a base do paralelogramo: ");
+                    base = scanner.nextDouble();
+                    System.out.print("Digite a altura do paralelogramo: ");
+                    altura = scanner.nextDouble();
+                    System.out.println("Área do paralelogramo: " + geometriaPlana.calcularAreaParalelogramo(base, altura));
+                    break;
 
-                    CalculadoraFormas calcularAreaQuadrado = new CalculadoraFormas(base);
-                    System.out.println("A área da forma plana é: " + super.calcularAreaQuadrado);
+                    case 8: 
+                    System.out.println("Digite o raio do círculo: ")
+                    raio = scanner.nextDouble();
+                    System.out.println("Área do círculo: " + geometriaPlana.calcularAreaCirculo(raio));
 
-                        break;
                     default:
                     System.err.println("Opção inválida!");
                         break;
@@ -123,8 +133,9 @@ public class Main extends CalculadoraFormas {
                     case 1:
                         System.out.println("Digite o lado do quadrado:");
                         double ladoQuadrado = scanner.nextDouble();
-                        System.out.println("A área do quadrado é: " + CalculadoraFormas.calcularAreaQuadrado(ladoQuadrado));
+                        System.out.println("A área do quadrado é: " + calcularAreaQuadrado(ladoQuadrado));
                         break;
+                        
                     case 2:
                         System.out.println("Digite a base do retângulo:");
                         double baseRetangulo = scanner.nextDouble();
@@ -132,6 +143,7 @@ public class Main extends CalculadoraFormas {
                         double alturaRetangulo = scanner.nextDouble();
                         System.out.println("A área do retângulo é: " + CalculadoraFormas.calcularAreaRetangulo(baseRetangulo, alturaRetangulo));
                         break;
+
                     case 3:
                         System.out.println("Digite a base do triângulo:");
                         double baseTriangulo = scanner.nextDouble();
@@ -139,6 +151,7 @@ public class Main extends CalculadoraFormas {
                         double alturaTriangulo = scanner.nextDouble();
                         System.out.println("A área do triângulo é: " + CalculadoraFormas.calcularAreaTriangulo(baseTriangulo, alturaTriangulo));
                         break;
+
                     case 4:
                         System.out.println("Digite a base maior do trapézio:");
                         double baseMaiorTrapezio = scanner.nextDouble();
@@ -148,6 +161,7 @@ public class Main extends CalculadoraFormas {
                         double alturaTrapezio = scanner.nextDouble();
                         System.out.println("A área do trapézio é: " + CalculadoraFormas.calcularAreaTrapezio(baseMaiorTrapezio, baseMenorTrapezio, alturaTrapezio));
                         break;
+
                     case 5:
                         System.out.println("Digite a diagonal maior do losango:");
                         double diagonalMaiorLosango = scanner.nextDouble();
@@ -155,6 +169,7 @@ public class Main extends CalculadoraFormas {
                         double diagonalMenorLosango = scanner.nextDouble();
                         System.out.println("A área do losango é: " + CalculadoraFormas.calcularAreaLosangulo(diagonalMaiorLosango, diagonalMenorLosango));
                         break;
+
                     case 6:
                         System.out.println("Digite a base do paralelogramo:");
                         double baseParalelogramo = scanner.nextDouble();
@@ -162,8 +177,9 @@ public class Main extends CalculadoraFormas {
                         double alturaParalelogramo = scanner.nextDouble();
                         System.out.println("A área do paralelogramo é: " + CalculadoraFormas.calcularAreaParalelogramo(baseParalelogramo, alturaParalelogramo));
                         break;
+
                     default:
-                        System.out.println("Opção inválida.");
+                        System.err.println("Opção inválida.");
                     break;
                 }
             }
