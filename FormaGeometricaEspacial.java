b998d9249e73f65cbf63080e538606314d875927
Anna Lia Lira <annalialira@gmail.com>
2024-06-05 21:26:43 -0300

    quase!

--- FormaGeometricaEspacial.java
+++ FormaGeometricaEspacial.java
@@ -1,25 +1,89 @@
 // Classe filha para formas geométricas espaciais
 
-class FormaGeometricaEspacial extends FormaGeometrica {
-    double base;
-    double altura;
-    double profundidade;
+class FormaGeometricaEspacial {
 
-    // construtor
-    public FormaGeometricaEspacial(double base, double altura, double profundidade) {
-        this.base = base;
-        this.altura = altura;
-        this.profundidade = profundidade;
+    // Volume PRISMA
+    public double calcularVolumePrisma(double base, double altura, double profundidade) {
+        double volume = base * altura * profundidade;
+        return volume;
     }
 
-    // Método para calcular a área
-    double calcularArea() {
-        // Área paralelepípedo
-        return 2 * (base * altura + base * profundidade + altura * profundidade);
+    // Área PRISMA
+    public double calcularAreaSuperficiePrisma(double base, double altura, double profundidade) {
+        double areaBase = base * altura;
+        double areaLateral = 2 * (base * profundidade + altura * profundidade);
+        double areaTotal = 2 * areaBase + areaLateral;
+        return areaTotal;
     }
-
-    // Método para calcular o volume
-    double calcularVolume() {
-        return base * altura * profundidade;
+ 
+    // Volume CILINDRO
+    public double calcularVolumeCilindro(double raio, double altura) {
+        double volume = Math.PI * raio * raio * altura;
+        return volume;
+    }
+ 
+    // Área CILINDRO
+    public double calcularAreaSuperficieCilindro(double raio, double altura) {
+        double areaBase = Math.PI * raio * raio;
+        double areaLateral = 2 * Math.PI * raio * altura;
+        double areaTotal = 2 * areaBase + areaLateral;
+        return areaTotal;
+    }
+ 
+    // Volume PIRÂMIDE 
+    public double calcularVolumePiramide(double base, double altura) {
+        double volume = (1.0 / 3.0) * base * altura;
+        return volume;
+    }
+ 
+    // Área PIRÂMIDE
+    public double calcularAreaSuperficiePiramide(double base, double altura, double apotema) {
+        double areaBase = base * base;
+        double areaLateral = 2 * base * apotema;
+        double areaTotal = areaBase + areaLateral;
+        return areaTotal;
+    }
+ 
+    // Volume ESFERA
+    public double calcularVolumeEsfera(double raio) {
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
+        return volume;
+    }
+ 
+    // Área ESFERA
+    public double calcularAreaSuperficieEsfera(double raio) {
+        double area = 4 * Math.PI * Math.pow(raio, 2);
+        return area;
+    }
+ 
+    // Volume CONE
+    public double calcularVolumeCone(double raio, double altura) {
+        double volume = (1.0 / 3.0) * Math.PI * Math.pow(raio, 3) * altura;
+        return volume;
+    }
+ 
+    // Área CONE
+    public double calcularAreaSuperficieCone(double raio, double altura) {
+        double areaBase = Math.PI * Math.pow(raio, 2);
+        double geratriz = Math.sqrt(Math.pow(raio, 2) + Math.pow(altura, 2));
+        double areaLateral = Math.PI * raio * geratriz;
+        double areaTotal = areaBase + areaLateral;
+        return areaTotal;
+    }
+ 
+    // Volume CONE
+    public double calcularVolumeTronco(double raioMaior, double raioMenor, double altura) {
+        double volume = (1.0 / 3.0) * Math.PI * altura * (Math.pow(raioMaior, 2) + Math.pow(raioMenor, 2) + raioMaior * raioMenor);
+        return volume;
+    }
+ 
+    // Área CONE
+    public double calcularAreaSuperficieTronco(double raioMaior, double raioMenor, double altura) {
+        double geratriz = Math.sqrt(Math.pow((raioMaior - raioMenor), 2) + Math.pow(altura, 2));
+        double areaBaseMaior = Math.PI * Math.pow(raioMaior, 2);
+        double areaBaseMenor = Math.PI * Math.pow(raioMenor, 2);
+        double areaLateral = Math.PI * geratriz * (raioMaior + raioMenor);
+        double areaTotal = areaBaseMaior + areaBaseMenor + areaLateral;
+        return areaTotal;
     }
 }
--- Main.java
+++ Main.java
@@ -5,25 +5,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CalculadoraFormas geometriaPlana = new CalculadoraFormas();
-        FormaGeometricaEspacial geometricaEspacial = new FormaGeometricaEspacial(0, 0, 0);
+        FormaGeometricaEspacial geometriaEspacial = new FormaGeometricaEspacial();
         //CalculadoraFormas calculadoraFormas = new CalculadoraFormas();
         //FormaGeometrica formaGeometrica = new FormaGeometrica();
         //FormaGeometricaEspacial formaGeometricaEspacial = new FormaGeometricaEspacial();
         //FormaGeometricaPlana formaGeometricaPlana = new FormaGeometricaPlana();
-        
+        System.out.println("\nSeja bem vindo a calculadora de geoometria plana e espacial!");
 
         while (true) {
             System.out.println("\nEscolha uma opção:");
             System.out.println("1. Calcular área de forma geométrica plana");
             System.out.println("2. Calcular área e volume de forma geométrica espacial");
-            System.out.println("3. Calcular área de formas geométricas especiais");
-            System.out.println("4. Encerrar o programa\n");
+            System.out.println("3. Encerrar o programa\n");
 
             int opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-                System.out.println("Escolha a forma geométrica plana:");
+                System.out.println("\nEscolha a forma geométrica plana:");
                 System.out.println("1. Quadrado");
                 System.out.println("2. Retângulo");
                 System.out.println("3. Triângulo");
@@ -33,13 +32,12 @@ public class Main {
                 System.out.println("7. Paralelogramo");
                 System.out.println("8. Círculo");
                 int opcao1 = scanner.nextInt();
-                System.out.println("Forma escolhida: "+opcao1+".");
+                System.out.println("\nForma escolhida: "+opcao1+".");
 
-                double diagonalMaior, diagonalMenor, raio;
 
                 switch (opcao1) {
                     case 1:
-                    System.out.println("Digite o valor do lado:");
+                    System.out.println("\nDigite o valor do lado:");
                     double lado = scanner.nextDouble();
 
                     CalculadoraFormas calcularAreaQuadrado = new CalculadoraFormas();
@@ -48,7 +46,7 @@ public class Main {
 
         
                     case 2:
-                    System.out.print("Digite a base do retângulo: ");
+                    System.out.print("\nDigite a base do retângulo: ");
                     double base = scanner.nextDouble();
                     System.out.print("Digite a altura do retângulo: ");
                     double altura = scanner.nextDouble();
@@ -56,7 +54,7 @@ public class Main {
                         break;
 
                     case 3:
-                    System.out.print("Digite a base do triângulo: ");
+                    System.out.print("\nDigite a base do triângulo: ");
                     base = scanner.nextDouble();
                     System.out.print("Digite a altura do triângulo: ");
                     altura = scanner.nextDouble();
@@ -64,13 +62,13 @@ public class Main {
                         break;
                     
                     case 4:
-                    System.out.print("Digite o lado do triângulo equilátero: ");
+                    System.out.print("\nDigite o lado do triângulo equilátero: ");
                     lado = scanner.nextDouble();
                     System.out.println("Área do triângulo equilátero: " + geometriaPlana.calcularAreaTrianguloEquilatero(lado));
                         break;
 
                     case 5:
-                    System.out.print("Digite a base maior do trapézio: ");
+                    System.out.print("\nDigite a base maior do trapézio: ");
                     base = scanner.nextDouble();
                     System.out.print("Digite a base menor do trapézio: ");
                     double baseMenor = scanner.nextDouble();
@@ -79,15 +77,15 @@ public class Main {
                     System.out.println("Área do trapézio: " + geometriaPlana.calcularAreaTrapezio(base, baseMenor, altura));
 
                     case 6:
-                    System.out.print("Digite a diagonal maior do losango: ");
-                    diagonalMaior = scanner.nextDouble();
+                    System.out.print("\nDigite a diagonal maior do losango: ");
+                    double diagonalMaior = scanner.nextDouble();
                     System.out.print("Digite a diagonal menor do losango: ");
-                    diagonalMenor = scanner.nextDouble();
+                    double diagonalMenor = scanner.nextDouble();
                     System.out.println("Área do losango: " + geometriaPlana.calcularAreaLosango(diagonalMaior, diagonalMenor));
                     break;
 
                     case 7:
-                    System.out.print("Digite a base do paralelogramo: ");
+                    System.out.print("\nDigite a base do paralelogramo: ");
                     base = scanner.nextDouble();
                     System.out.print("Digite a altura do paralelogramo: ");
                     altura = scanner.nextDouble();
@@ -95,93 +93,105 @@ public class Main {
                     break;
 
                     case 8: 
-                    System.out.println("Digite o raio do círculo: ")
-                    raio = scanner.nextDouble();
+                    System.out.println("\nDigite o raio do círculo: ");
+                    double raio = scanner.nextDouble();
                     System.out.println("Área do círculo: " + geometriaPlana.calcularAreaCirculo(raio));
 
                     default:
-                    System.err.println("Opção inválida!");
+                    System.err.println("\nOpção inválida!");
                         break;
                 }
                     break;
 
                 case 2:
-                System.out.println("Digite a base da forma espacial:");
-                double base = scanner.nextDouble();
-                System.out.println("Digite a altura da forma espacial:");
-                double altura = scanner.nextDouble();
-                System.out.println("Digite a profundidade da forma espacial:");
-                double profundidade = scanner.nextDouble();
-
-                FormaGeometricaEspacial formaEspacial = new FormaGeometricaEspacial(base, altura, profundidade);
-                System.out.println("A área da forma espacial é: " + formaEspacial.calcularArea());
-                System.out.println("O volume da forma espacial é: " + formaEspacial.calcularVolume());
-                    break;
-
-                case 3:
-                System.out.println("Escolha a forma geométrica especial:");
-                System.out.println("1. Quadrado");
-                System.out.println("2. Retângulo");
-                System.out.println("3. Triângulo");
-                System.out.println("4. Trapézio");
-                System.out.println("5. Losango");
-                System.out.println("6. Paralelogramo"); 
+                System.out.println("\nEscolha a forma geométrica especial:");
+                System.out.println("1. Prismas");
+                System.out.println("2. Cilindro");
+                System.out.println("3. Pirâmide");
+                System.out.println("4. Esfera");
+                System.out.println("5. Cone");
+                System.out.println("6. Tronco de cone");
                 
                 int opcaoFormaEspecial = scanner.nextInt();
 
                 switch (opcaoFormaEspecial) {
                     case 1:
-                        System.out.println("Digite o lado do quadrado:");
-                        double ladoQuadrado = scanner.nextDouble();
-                        System.out.println("A área do quadrado é: " + calcularAreaQuadrado(ladoQuadrado));
-                        break;
+                        System.out.print("\nDigite a base do prisma: ");
+                        double basePrisma = scanner.nextDouble();
+                        System.out.print("Digite a altura do prisma: ");
+                        double alturaPrisma = scanner.nextDouble();
+                        System.out.print("Digite a profundidade do prisma: ");
+                        double profundidadePrisma = scanner.nextDouble();
+                        double volumePrisma = geometriaEspacial.calcularVolumePrisma(basePrisma, alturaPrisma, profundidadePrisma);
+                        double areaSuperficiePrisma = geometriaEspacial.calcularAreaSuperficiePrisma(basePrisma, alturaPrisma, profundidadePrisma);
+                        System.out.println("Área da superfície do prisma: " + areaSuperficiePrisma);
+                        System.out.println("Volume do prisma: " + volumePrisma);
+                    break;
                         
                     case 2:
-                        System.out.println("Digite a base do retângulo:");
-                        double baseRetangulo = scanner.nextDouble();
-                        System.out.println("Digite a altura do retângulo:");
-                        double alturaRetangulo = scanner.nextDouble();
-                        System.out.println("A área do retângulo é: " + CalculadoraFormas.calcularAreaRetangulo(baseRetangulo, alturaRetangulo));
-                        break;
+                        System.out.print("\nDigite o raio do cilindro: ");
+                        double raioCilindro = scanner.nextDouble();
+                        System.out.print("Digite a altura do cilindro: ");
+                        double alturaCilindro = scanner.nextDouble();
+                        double volumeCilindro = geometriaEspacial.calcularVolumeCilindro(raioCilindro, alturaCilindro);
+                        double areaSuperficieCilindro = geometriaEspacial.calcularAreaSuperficieCilindro(raioCilindro, alturaCilindro);
+                        System.out.println("Área da superfície do cilindro: " + areaSuperficieCilindro);
+                        System.out.println("Volume do cilindro: " + volumeCilindro);
+                    break;
 
                     case 3:
-                        System.out.println("Digite a base do triângulo:");
-                        double baseTriangulo = scanner.nextDouble();
-                        System.out.println("Digite a altura do triângulo:");
-                        double alturaTriangulo = scanner.nextDouble();
-                        System.out.println("A área do triângulo é: " + CalculadoraFormas.calcularAreaTriangulo(baseTriangulo, alturaTriangulo));
-                        break;
+                        System.out.print("\nDigite a base da pirâmide: ");
+                        double basePiramide = scanner.nextDouble();
+                        System.out.print("Digite a altura da pirâmide: ");
+                        double alturaPiramide = scanner.nextDouble();
+                        System.out.print("Digite a apotema da pirâmide: ");
+                        double apotemaPiramide = scanner.nextDouble();
+                        double volumePiramide = geometriaEspacial.calcularVolumePiramide(basePiramide, alturaPiramide);
+                        double areaSuperficiePiramide = geometriaEspacial.calcularAreaSuperficiePiramide(basePiramide, alturaPiramide, apotemaPiramide);
+                        System.out.println("Área da superfície da pirâmide: " + areaSuperficiePiramide);
+                        System.out.println("Volume da pirâmide: " + volumePiramide);
+                    break;
 
                     case 4:
-                        System.out.println("Digite a base maior do trapézio:");
-                        double baseMaiorTrapezio = scanner.nextDouble();
-                        System.out.println("Digite a base menor do trapézio:");
-                        double baseMenorTrapezio = scanner.nextDouble();
-                        System.out.println("Digite a altura do trapézio:");
-                        double alturaTrapezio = scanner.nextDouble();
-                        System.out.println("A área do trapézio é: " + CalculadoraFormas.calcularAreaTrapezio(baseMaiorTrapezio, baseMenorTrapezio, alturaTrapezio));
-                        break;
+                        System.out.print("\nDigite o raio da esfera: ");
+                        double raioEsfera = scanner.nextDouble();
+                        double volumeEsfera = geometriaEspacial.calcularVolumeEsfera(raioEsfera);
+                        double areaSuperficieEsfera = geometriaEspacial.calcularAreaSuperficieEsfera(raioEsfera);
+                        System.out.println("Área da superfície da esfera: " + areaSuperficieEsfera);
+                        System.out.println("Volume da esfera: " + volumeEsfera);
+                    break;
 
                     case 5:
-                        System.out.println("Digite a diagonal maior do losango:");
-                        double diagonalMaiorLosango = scanner.nextDouble();
-                        System.out.println("Digite a diagonal menor do losango:");
-                        double diagonalMenorLosango = scanner.nextDouble();
-                        System.out.println("A área do losango é: " + CalculadoraFormas.calcularAreaLosangulo(diagonalMaiorLosango, diagonalMenorLosango));
-                        break;
+                        System.out.print("\nDigite o raio do cone: ");
+                        double raioCone = scanner.nextDouble();
+                        System.out.print("Digite a altura do cone: ");
+                        double alturaCone = scanner.nextDouble();
+                        double volumeCone = geometriaEspacial.calcularVolumeCone(raioCone, alturaCone);
+                        double areaSuperficieCone = geometriaEspacial.calcularAreaSuperficieCone(raioCone, alturaCone);
+                        System.out.println("Área da superfície do cone: " + areaSuperficieCone);
+                        System.out.println("Volume do cone: " + volumeCone);
+                    break;
 
                     case 6:
-                        System.out.println("Digite a base do paralelogramo:");
-                        double baseParalelogramo = scanner.nextDouble();
-                        System.out.println("Digite a altura do paralelogramo:");
-                        double alturaParalelogramo = scanner.nextDouble();
-                        System.out.println("A área do paralelogramo é: " + CalculadoraFormas.calcularAreaParalelogramo(baseParalelogramo, alturaParalelogramo));
-                        break;
-
+                        System.out.print("\nDigite o raio maior do tronco de cone: ");
+                        double raioMaiorTronco = scanner.nextDouble();
+                        System.out.print("Digite o raio menor do tronco de cone: ");
+                        double raioMenorTronco = scanner.nextDouble();
+                        System.out.print("Digite a altura do tronco de cone: ");
+                        double alturaTronco = scanner.nextDouble();
+                        double volumeTronco = geometriaEspacial.calcularVolumeTronco(raioMaiorTronco, raioMenorTronco, alturaTronco);
+                        double areaSuperficieTronco = geometriaEspacial.calcularAreaSuperficieTronco(raioMaiorTronco, raioMenorTronco, alturaTronco);
+                        System.out.println("Área da superfície do tronco de cone: " + areaSuperficieTronco);
+                        System.out.println("Volume do tronco de cone: " + volumeTronco);
+                    break;
+                        
                     default:
-                        System.err.println("Opção inválida.");
+                        System.err.println("\nOpção inválida!");
                     break;
                 }
+
+            case 3:
+            System.out.println("\nFinalizando programa!");
             }
         }
     }
